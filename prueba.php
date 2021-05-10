af77b483501708c9bd2c4e010017f48de9ac1419
Charlin Ignacio <ignaciocharlin1995@gmail.com>
2021-05-10 15:15:08 -0300

    Modificacion de las card con la colocacion del los tipos

--- prueba.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-header('location:index.php');
-
-$resultado = $_POST['nombre'] .' '. $_POST['tipo'].' '.$_POST['img'];
-echo $resultado; //haciendo este echo estas respondiendo la solicitud ajax
-
-
-?>
--- secciones/body.php
+++ secciones/body.php
@@ -5,21 +5,35 @@
     $sentencia->execute();
     $resultado = $sentencia->fetchAll(PDO::FETCH_OBJ);
 
-
+    echo '<div class="container cards">';
      foreach ($resultado as $pokemon){
 ?>
-
-
-
 <div class="card mt-5 container" style="width: 18rem;">
-    <img src="img-temporal/<?php echo $pokemon->imagen ?>" class="card-img-top" alt="...">
+    <img src="<?php echo $pokemon->imagen ?>" class="card-img-top imagenes-pokemon" alt="imagenes-pokemon">
     <div class="card-body">
-        <h5 class="card-title"><?php echo $pokemon->nombre ?></h5>
-        <p class="card-text"><?php echo $pokemon->tipo ?></p>
+        <h5 class="card-title text-center titulo-card"><?php echo $pokemon->nombre ?></h5>
+        <p class="card-text icono-tipo"><?php switch($pokemon->tipo){
+                case 'fire':
+                    echo '<img src="img/Type_Fuego.png">';
+                    break;
+                case 'water':
+                    echo '<img src="img/Type_Agua.png">';
+                    break;
+                case 'bug':
+                    echo '<img src="img/Type_Bicho.png">';
+                    break;
+                case 'grass':
+                    echo '<img src="img/Type_Planta.png">';
+                    break;
+                case 'normal':
+                    echo '<img src="img/Type_normal.png">';
+                    break;
+            }?></p>
     </div>
 </div>
         <?php
             }
+            echo '</div>'
         ?>
 
     <a href="model/agregarPokemon.php" class="btn btn-primary">Agregar Pokemon</a>
